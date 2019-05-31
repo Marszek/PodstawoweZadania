f4ed2b9c51f0bc82a88675b974c3752dd2a64b06
Marcin Wdowiak <mardowiak@gmail.com>
2019-05-31 09:17:44 +0200

    Program wyświetla pierwiastek z PI

---
+++ src/Zadanie_1_3.java
@@ -0,0 +1,6 @@
+public class Zadanie_1_3 {
+    public static void pierwiastekZPI(){
+        double wynik= Math.sqrt(Math.PI);
+        System.out.printf("Pierwiastek z PI to: "+"%1.13f\n", wynik);
+    }
+}
