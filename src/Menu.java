c644adbb2a0ecc216b22b79b6b9464601cabe5b8
Marcin Wdowiak <mardowiak@gmail.com>
2019-05-31 09:12:59 +0200

    Klasa zawierająca metodę wyświetlającą menu programu

---
+++ src/Menu.java
@@ -0,0 +1,13 @@
+public class Menu {
+
+    public static void mainMenu(){
+
+        System.out.println("Wybierz zadanie, ktore chcesz przeanalizowac:");
+        System.out.println("1: Obliczanie pola prostokata");
+        System.out.println("2: PI z 15 miejscami po przecinku");
+        System.out.println("3: Obliczanie pierwiastka z PI");
+        System.out.println("4: Obliczanie objetosci kuli");
+        System.out.println("5: Dzielenie bez reszty i przecinka");
+        System.out.println("6: Obliczanie reszty z dzielenia 2 liczb");
+    }
+}
